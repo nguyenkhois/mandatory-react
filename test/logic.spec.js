3f90c8ea45ede87b0a895d8389d225d9336d6fd0
Khoi Le <minarek@outlook.com>
2018-04-28 21:30:40 +0200

    Added testing result

--- test/logic.spec.js
+++ test/logic.spec.js
@@ -1,11 +1,18 @@
-import {makeMove, newGame} from '../src/logic';
+import { makeMove, newGame } from '../src/logic';
 
-test('first move works ok', () => {
-  const initial = newGame();
-  const expected = { state: 'plr2', board: [0,0,1,0,0,0,0,0,0], line: [] };
-  const result = makeMove(initial, 2);
-  expect(result).toEqual(expected);
-  expect(initial).toEqual(newGame()); // testing we didn't mutate entry state
+//Case 1
+test('Testing for makeMove() function - Case 1', () => {
+    const initial = newGame();
+    const expected = {
+        gameState: 'plr2',
+        gameBoard: [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        winLine: [],
+        gameOver: false,
+        stepNumber: 1
+    };
+    const result = makeMove(initial, 0);
+    expect(result).toEqual(expected);
+    expect(initial).toEqual(newGame()); // testing we didn't mutate entry state
 });
 
 // ...more tests to follow here! 
