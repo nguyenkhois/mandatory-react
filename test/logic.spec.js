655b24c30f02cf6985a36ba47d54b5c37fd3d7c5
Khoi Le <minarek@outlook.com>
2018-04-28 21:45:33 +0200

    Updated 3 testing cases

--- test/logic.spec.js
+++ test/logic.spec.js
@@ -1,7 +1,7 @@
 import { makeMove, newGame } from '../src/logic';
 
 //Case 1
-test('Testing for makeMove() function - Case 1', () => {
+test('makeMove() - Case 1 - First move', () => {
     const initial = newGame();
     const expected = {
         gameState: 'plr2',
@@ -17,7 +17,7 @@ test('Testing for makeMove() function - Case 1', () => {
 
 // ...more tests to follow here! 
 //Case 2
-test('Testing for makeMove() function - Case 2', () => {
+test('makeMove() - Case 2 - Player 1 won', () => {
     const initial = {
         gameState: 'plr1',
         gameBoard: [1, 2, 1, 2, 1, 2, 1, 2, 0],
@@ -29,6 +29,26 @@ test('Testing for makeMove() function - Case 2', () => {
         gameState: 'plr1won',
         gameBoard: [1, 2, 1, 2, 1, 2, 1, 2, 1],
         winLine: [0,4,8],
+        gameOver: true,
+        stepNumber: 9
+    };
+    const result = makeMove(initial, 8);
+    expect(result).toEqual(expected); // testing we didn't mutate entry state
+});
+
+//Case 3
+test('makeMove() - Case 3 - Game over. Nobody won', () => {
+    const initial = {
+        gameState: 'plr1',
+        gameBoard: [2, 1, 1, 1, 2, 2, 1, 2, 0],
+        winLine: [],
+        gameOver: false,
+        stepNumber: 8
+    };
+    const expected = {
+        gameState: 'draw',
+        gameBoard: [2, 1, 1, 1, 2, 2, 1, 2, 1],
+        winLine: [],
         gameOver: true,
         stepNumber: 9
     };
